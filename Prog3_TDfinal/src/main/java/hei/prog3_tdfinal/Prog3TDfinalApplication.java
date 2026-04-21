6737adfe771f88941a267828b8fc47e2cd4f286e
saviola24 <saviolafenitr@gmail.com>
2026-04-21 15:14:35 +0300

    part 2

---
+++ Prog3_TDfinal/src/main/java/hei/prog3_tdfinal/Prog3TDfinalApplication.java
@@ -0,0 +1,13 @@
+package hei.prog3_tdfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3TDfinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3TDfinalApplication.class, args);
+    }
+
+}
---
+++ Prog3_TDfinal/src/test/java/hei/prog3_tdfinal/Prog3TDfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.prog3_tdfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3TDfinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
